9b1479848a5c8b48177dd09c6378304161489b35
boyko <boyko.i.vasilev@gmail.com>
2025-01-23 21:42:59 +0200

    rollback

--- ssh.tf
+++ ssh.tf
@@ -20,7 +20,6 @@ resource "azapi_resource_action" "ssh_public_key_gen" {
 }
 
 resource "azapi_resource" "ssh_public_key" {
-  provider = azapi.alias1
   type      = "Microsoft.Compute/sshPublicKeys@2022-11-01"
   name      = random_pet.ssh_key_name.id
   location  = azurerm_resource_group.rg.location
